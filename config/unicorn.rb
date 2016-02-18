ee1c754542a512d839d11e0e68df506b276dab9c
nakamura-tsuyoshi <nakamura@ca-crowdfunding.com>
2016-02-18 22:24:26 +0900

    add unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,8 +4,7 @@ rails_env = ENV['RAILS_ENV'] || "development"
 worker_processes 2
 working_directory rails_root
 
-#listen 8080
-listen "#{rails_root}/tmp/#{rails_env}_unicorn.sock"
+listen 8080
 pid "#{rails_root}/tmp/#{rails_env}_unicorn.pid"
 
 stderr_path "#{rails_root}/log/#{rails_env}_unicorn_error.log"
